94e38dfa59df98618848b781fac8522b8a5a9daa
stalker-vishnu <stalker@MacBook-Air-stalker.local>
2018-03-12 13:26:02 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my Awesome site! </hq>")
+
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
